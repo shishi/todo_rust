880aa93cabd2fe0541681d573e2d329fbc6afa36
Shigenobu Nishikawa <shishi@srevo.net>
2021-09-01 01:19:54 +0900

    5-1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, ResponseError, get};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum MyError {}
+
+impl ResponseError for MyError {}
+
+#[get("/")]
+async fn index() -> Result<HttpResponse, MyError> {
+    let response_body = "Hello world";
+
+    Ok(HttpResponse::Ok().body(response_body))
+}
+
+#[actix_rt::main]
+async fn main() -> Result<(), actix_web::Error> {
+    HttpServer::new(move || App::new().service(index))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await?;
+
+    Ok(())
 }
